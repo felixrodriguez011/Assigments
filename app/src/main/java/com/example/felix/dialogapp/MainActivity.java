a15c61e2649b9eadc1eeb671f82f5e9983542e8a
felix <felix.rodriguez011@mymdc.net>
2018-06-25 22:10:47 -0400

    Assigment #5

--- app/src/main/java/com/example/felix/dialogapp/MainActivity.java
+++ app/src/main/java/com/example/felix/dialogapp/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.felix.dialogapp;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity
 implements MyDialogFragment.OnMyDialogConfirmedListener,
 DialogExit.OnMyDialogConfirmedListener,
         SignInDialogFragment.OnCredentialEnteredListener {
+    private static final String prefs = "userPrefs";
+    private static final String FILENAME = "Reminders";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +49,32 @@ DialogExit.OnMyDialogConfirmedListener,
                         .setAction("Action", null).show();
             }
         });
+
+        //Reading from file
+        FileInputStream fin = null;
+        BufferedReader bufferedReader = null;
+        StringBuilder temp = new StringBuilder();
+        try {
+            fin = openFileInput(FILENAME);
+            bufferedReader = new BufferedReader(new InputStreamReader(fin));
+            String line;
+            while( (line = bufferedReader.readLine()) != null){
+                temp.append(line).append("\n");
+            }
+            bufferedReader.close();
+            fin.close();
+            Toast toast = Toast.makeText(this, "Reminder:\n" + temp.toString(), Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void onBackPressed(){
+    public void onBackPressed() {
         DialogExit newFragment = DialogExit.newInstance();
         newFragment.show(getSupportFragmentManager(), "ExitApp");
 
@@ -77,18 +114,81 @@ DialogExit.OnMyDialogConfirmedListener,
         newFragment.show(getSupportFragmentManager(), "confirmDeleteArtPiece");
     }
 
-    public void signInDialog(View view) {
-        SignInDialogFragment newFragment = SignInDialogFragment.newInstance();
-        newFragment.show(getSupportFragmentManager(), "signInDialog");
-    }
-
     @Override
     public void authenticate(String username) {
-        if(username.equals("felix")){
+        if (username.equals("felix")) {
             Toast.makeText(this, "Authenticated, Welcome", Toast.LENGTH_LONG).show();
+
+            //Writing to shared preferences
+            SharedPreferences sharedPref = this.getSharedPreferences(prefs, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("userName", username);
+            editor.commit();
+
+            sayHello(username);
         } else {
             Toast.makeText(this, "Wrong username", Toast.LENGTH_LONG).show();
         }
 
+
+
     }
+
+    public void signInDialog(View view) {
+        //Reading from shared preferences
+        SharedPreferences sharedPref = this.getSharedPreferences(prefs, Context.MODE_PRIVATE);
+        //Reading the username from the shared preferences
+        String savedUserName = sharedPref.getString("userName", null);
+        if (savedUserName == null) {
+            SignInDialogFragment newFragment = SignInDialogFragment.newInstance();
+            newFragment.show(getSupportFragmentManager(), "signInDialog");
+        } else {
+            sayHello(savedUserName);
+        }
+
+
+    }
+
+    private void sayHello(String userName) {
+        Toast toast = Toast.makeText(this, "Welcome " + userName + "!", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.TOP, 10, 10);
+        toast.show();
+    }
+
+    public void addReminder(View view) {
+        //Writing to file
+        EditText reminderText = (EditText) findViewById(R.id.reminderText);
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(FILENAME, Context.MODE_PRIVATE | Context.MODE_APPEND);
+            outputStream.write(reminderText.getText().toString().concat("\n").getBytes());
+            outputStream.close();
+            Toast.makeText(this, "Reminder added!", Toast.LENGTH_LONG).show();
+            reminderText.getText().clear();
+        } catch (Exception e) {
+            Log.w("WARNING", "Cannot save reminder..");
+        }
+    }
+
+    public void addThing(View view) {
+        final AppDatabase mDb = AppDatabase.getInstance(MainActivity.this);
+        final EditText nameText = (EditText) findViewById(R.id.nameText);
+        final EditText attributeText = (EditText) findViewById(R.id.attributeText);
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(final Void ... params ) {
+                mDb.thingModel().insert(new Thing(nameText.getText().toString(), attributeText.getText().toString()));
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute( final Void result ) {
+                nameText.getText().clear();
+                attributeText.getText().clear();
+            }
+        }.execute();
+
+    }
+
 }
